0209f7db2f379d825d7f4e70488dd1f438111afb
Sebastian Sacharczuk <sebsach.com@gmail.com>
2023-04-28 20:01:49 +0200

    Adding method 'toSvg' to a class 'Segment'

--- LAB1/src/Main.java
+++ LAB1/src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         Point point1 = new Point(8, 6), point2 = new Point(0, 0);
         Segment segment = new Segment(point1, point2);
         System.out.println(segment.length());
+        System.out.println(segment.toSvg());
     }
 }
--- LAB1/src/Segment.java
+++ LAB1/src/Segment.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 public class Segment {
     private Point p1, p2;
 
@@ -8,6 +10,10 @@ public class Segment {
 
     public double length(){
         return Math.sqrt((p1.x - p2.x)*(p1.x - p2.x) + (p1.y - p2.y)*(p1.y - p2.y));
+    }
+
+    public String toSvg(){
+        return String.format("<line x1=\"%f\" y1=\"%f\" x2=\"%f\" y2=\"%f\" style=\"stroke:rgb(255,0,0);stroke-width:2\" />", p1.x, p1.y, p2.x, p2.y);
     }
 
 }
